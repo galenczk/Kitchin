6c5624928059ad4b81a6d339c68652ec0671781c
galenczk <ciszekg@oregonstate.edu>
2022-10-15 09:58:43 -0700

    Tailwind works.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 // Import dependencies.
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+//import "./App.css";
 
 // Import pages.
 import WelcomePage from "./pages/WelcomePage";
 import PreferencesPage from "./pages/PreferencesPage";
 import SearchPage from "./pages/SearchPage";
-import ResultsPage from "./pages/ResultsPage"
+import ResultsPage from "./pages/ResultsPage";
 //import RecipePage from "./pages/RecipePage"
 
 function App() {
   return (
-    <div className="App">
+    <div class="flex flex-col min-h-screen justify-between">
       <Router>
-        <header className="App-header">
+        <header class="bg-gray-800 text-white text-3xl p-2">
           <h2>kitchin</h2>
         </header>
 
-        <main className="App-main">
+        <main 
+          class="flex flex-col text-center mb-auto"
+        >
           <Routes>
             <Route path="/" exact element={<WelcomePage />} />
             <Route path="/preferences" element={<PreferencesPage />} />
-            <Route path="/search" element={<SearchPage />}/>
-            <Route path="/results" element={<ResultsPage />}/>
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/results" element={<ResultsPage />} />
           </Routes>
         </main>
 
-        <footer>
+        <footer
+          class="text-center bg-gray-300 p-0.5"
+        >
           <p>Galen Ciszek &copy; 2022</p>
         </footer>
       </Router>
--- src/components/Button.js
+++ src/components/Button.js
@@ -4,14 +4,15 @@ import React from "react"
 export default function Button(props){
 
   return (
-    <button 
-    className={props.class}
-    onClick={props.onClick}
-    type={props.type}
+    <button
+
+      className={props.class}
+      onClick={props.onClick}
+      type={props.type}
     >
       {props.label}
     </button>
-  )
+  );
 }
 
 
