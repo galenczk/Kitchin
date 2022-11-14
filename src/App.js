e715d7d471a7f3674624d4afabbca797efa4b044
galenczk <ciszekg@oregonstate.edu>
2022-11-13 17:42:59 -0800

    Random bg image works.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 // Import dependencies
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import image0 from "./images/0.avif";
+import image1 from "./images/0.avif";
+import image2 from "./images/0.avif";
+import image3 from "./images/0.avif";
+import image4 from "./images/0.avif";
+import image5 from "./images/0.avif";
+import image6 from "./images/0.avif";
+import image7 from "./images/0.avif";
+import image8 from "./images/0.avif";
+import image9 from "./images/0.avif";
 
 // Import pages
 import WelcomePage from "./pages/WelcomePage";
@@ -12,26 +24,37 @@ import ResultsPage from "./pages/ResultsPage";
 
 // Page function
 function App() {
+
+  const [imageNumber, setImageNumber] = useState(); 
+  
+
+   useEffect(() => {
+     const callRNG = async () =>{
+      const response = await axios.get("https://testmicro.vercel.app")
+      let number = await response.data
+      number = await number % 10
+      setImageNumber(await number)
+      console.log(imageNumber)
+     }
+     callRNG()
+   }, []);
+
   // DOM return
   return (
     <div class="flex flex-col min-h-screen">
       <Router>
         <header class="bg-slate-800 text-white text-3xl p-2">
-          <h2>Kitchin</h2> 
+          <h2>Kitchin</h2>
         </header>
 
-        <main class="flex flex-col mb-auto items-center flex-grow bg-0">
+        <main class={`flex flex-col mb-auto items-center flex-grow bg-${imageNumber}`} >
           <Routes>
             <Route path="/" exact element={<WelcomePage />} />
             <Route path="/preferences" element={<PreferencesPage />} />
             <Route path="/search" element={<SearchPage />} />
             <Route path="/results" element={<ResultsPage />} />
-            
-            
           </Routes>
         </main>
-
-        
 
         <footer class="text-center bg-gray-300 p-0.5 ">
           <p>Galen Ciszek &copy; 2022</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,17 @@ module.exports = {
         cyberYellow: "#fbd00e",
       },
       backgroundImage: {
-        "0": "url('/images/0.avif')"
-      }
+        "0": "url('./images/0.avif')",
+        "1": "url('./images/1.avif')",
+        "2": "url('./images/2.avif')",
+        "3": "url('./images/3.avif')",
+        "4": "url('./images/4.avif')",
+        "5": "url('./images/5.avif')",
+        "6": "url('./images/6.avif')",
+        "7": "url('./images/7.avif')",
+        "8": "url('./images/8.avif')",
+        "9": "url('./images/9.avif')"
+      },
     },
   },
   plugins: [],
